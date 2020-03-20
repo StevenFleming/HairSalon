c28039cca9fa2ccf0e7125255bb13062594e9ad4
Student <example@example.com>
2020-03-20 16:01:41 -0700

    changes naming convention of Client.Stylist to Client.Stylists and updates forms accordingly. Also adds in Edit for Clients

--- Models/Client.cs
+++ Models/Client.cs
@@ -9,7 +9,7 @@ namespace HairSalon.Models
         public string Name {get; set;}
         public int ClientId {get; set;}
         public int StylistId {get; set;}
-        public virtual Stylist Stylist {get; set;}
+        public virtual Stylist Stylists {get; set;}
 
     }
 }
