cf77c7ac905c47d2c684970a263cc07808c93d9f
Student <example@example.com>
2020-03-20 20:20:57 -0700

    Displays the details for each of the Stylists Clients

--- Controllers/StylistsController.cs
+++ Controllers/StylistsController.cs
@@ -39,6 +39,8 @@ namespace HairSalon.Controllers
     public ActionResult Details(int id)
     {
         Stylist thisStylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+        
+        thisStylist.Clients = _db.Clients.Where(client => client.StylistId == id).ToList();
         return View(thisStylist);
     }
 
